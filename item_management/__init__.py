1e29fbfba087b6727b19bc27e74c7768ef7776ec
Xi Yang <starry.yohee@gmail.com>
2020-11-03 17:35:10 +0800

    first success version

--- item_management/__init__.py
+++ item_management/__init__.py
@@ -1,8 +1,8 @@
-import sqlite3
-import click
+import shelve
+
 from flask import Flask, current_app, g
-from flask.cli import with_appcontext
 from flask_restful import Resource, Api, reqparse
+
 
 app = Flask(__name__)
 
@@ -10,12 +10,7 @@ api = Api(app)
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
+        g.db = shelve.open("item.db")
     return g.db
 
 
@@ -27,10 +22,52 @@ def close_db(e=None):
 
 class ItemList(Resource):
     def get(self):
+        shelf = get_db()
+        keys = list(shelf.keys())
 
-        return {}
+        items = []
+
+        for key in keys:
+            items.append(shelf[key])
+        
+        return {'message': 'Success', 'data':items}, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('identifier', required=True)
+        parser.add_argument('name', required=True)
+        parser.add_argument('brand')
+        parser.add_argument('item_type')
+        parser.add_argument('expire_date', required=True)
+        parser.add_argument('desire')
+        parser.add_argument('sales')
+
+        args = parser.parse_args()
+
+        shelf = get_db()
+        shelf[args['identifier']] = args
+
+        return {'message': 'Item added', 'data': args}, 201
 
 class Item(Resource):
-    def get(self,identifier):
+    def get(self,identifier:str):
+        shelf = get_db()
+
+        if not (identifier in shelf):
+            return {'message': 'Item not found', 'data': {}}, 404
+
+        return {'message':'Item found', 'data': shelf[identifier]}, 200
+
+    def delete(self, identifier):
+        shelf = get_db()
+
+        if not(identifier in shelf):
+            return {'message': 'Item not found', 'data': {}}, 404
+
+        del shelf[identifier]
+        return '', 204
+
         
-        return {}
+api.add_resource(ItemList, '/items')
+api.add_resource(Item, '/item/<string:identifier>')
